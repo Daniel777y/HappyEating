a7dd4e584279100dfef4df168ebe3668561e1f79
Daniel <mailreceiving@qq.com>
2020-12-05 23:19:32 +0800

    修正部分代码格式

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,45 +1,45 @@
 export default {
-  // Global page headers (https://go.nuxtjs.dev/config-head)
-  head: {
-    title: 'client',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+    // Global page headers (https://go.nuxtjs.dev/config-head)
+    head: {
+        title: 'client',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+
+    // Global CSS (https://go.nuxtjs.dev/config-css)
+    css: [
+        '~/assets/css/transition.css',
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    '~/assets/css/transition.css',
-  ],
+    // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+    plugins: [
+    ],
 
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+    // Auto import components (https://go.nuxtjs.dev/config-components)
+    components: true,
 
-  // Auto import components (https://go.nuxtjs.dev/config-components)
-  components: true,
+    // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+    buildModules: [
+    ],
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+    // Modules (https://go.nuxtjs.dev/config-modules)
+    modules: [
+        // https://go.nuxtjs.dev/bootstrap
+        'bootstrap-vue/nuxt',
+        '@nuxtjs/axios',
+    ],
 
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
-    '@nuxtjs/axios',
-  ],
+    axios: {
+        baseURL: 'http://localhost:8000/api',
+    },
 
-  axios: {
-    baseURL: 'http://localhost:8000/api',
-  },
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
+    // Build Configuration (https://go.nuxtjs.dev/config-build)
+    build: {
+    }
 }
--- client/pages/recipes/index.vue
+++ client/pages/recipes/index.vue
@@ -1,61 +1,55 @@
 <template>
-  <main class="container mt-5">
-    <div class="row">
-      <div class="col-12 text-right mb-4">
-        <div class="d-flex justify-content-between">
-          <h3>吃货天堂</h3>
-          <nuxt-link to="/recipes/add" class="btn btn-info">添加食谱</nuxt-link>
+    <main class="container mt-5">
+        <div class="row">
+            <div class="col-12 text-right mb-4">
+                <div class="d-flex justify-content-between">
+                    <h3>吃货天堂</h3>
+                    <nuxt-link to="/recipes/add" class="btn btn-info">添加食谱</nuxt-link>
+                </div>
+            </div>
+            <template v-for="recipe in recipes">
+                <div :key="recipe.id" class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                    <recipe-card :onDelete="deleteRecipe" :recipe="recipe"></recipe-card>
+                </div>
+            </template>
         </div>
-      </div>
-      <template v-for="recipe in recipes">
-        <div :key="recipe.id" class="col-lg-3 col-md-4 col-sm-6 mb-4">
-          <recipe-card :onDelete="deleteRecipe" :recipe="recipe"></recipe-card>
-        </div>
-      </template>
-    </div>
-  </main>
+    </main>
 </template>
   
 <script>
 import RecipeCard from "~/components/RecipeCard.vue";
-  
-  
-export default {
-  head() {
-    return {
-      title: "食谱列表"
-    };
-  },
-  components: {
-    RecipeCard
-  },
 
-  async asyncData({ $axios, params }) {
-    try {
-      let recipes = await $axios.$get(`/recipes/`);
-      return { recipes };
-    } catch (e) {
-      return { recipes: [] };
-    }
-  },
-  data() {
-    return {
-       recipes: []
-    };
-  },
-  methods: {
-      async deleteRecipe(recipe_id) {
-      try {
-        if (confirm('确认要删除吗？')) {
-          await this.$axios.$delete(`/recipes/${recipe_id}/`);
-          let newRecipes = await this.$axios.$get("/recipes/");
-          this.recipes = newRecipes;
+
+export default {
+    head() {
+        return { title: "食谱列表" };
+    },
+    components: { RecipeCard },
+
+    async asyncData({ $axios, params }) {
+        try {
+            let recipes = await $axios.$get(`/recipes/`);
+            return { recipes };
+        } catch (e) {
+            return { recipes: [] };
         }
-      } catch (e) {
-        console.log(e);
-      }
+    },
+    data() {
+        return { recipes: [] };
+    },
+    methods: {
+        async deleteRecipe(recipe_id) {
+            try {
+                if (confirm('确认要删除吗？')) {
+                    await this.$axios.$delete(`/recipes/${recipe_id}/`);
+                    let newRecipes = await this.$axios.$get("/recipes/");
+                    this.recipes = newRecipes;
+                }
+            } catch (e) {
+                console.log(e);
+            }
+        }
     }
-  }
 };
 </script>
   
